b7d7fc6a975dfe23a1fc03a28cea65c1ff4e6ebf
elena-buscaroli <ele.buscaroli@gmail.com>
2022-05-15 16:26:54 +0200

    Reticulate configuration

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,6 @@
-.OnLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname)
+.onLoad <- function(libname, pkgname) {
+  # reticulate::configure_environment(pkgname, force=T)
+  reticulate::install_miniconda(path = reticulate::miniconda_path(), update = TRUE, force = FALSE)
+  reticulate::py_install("pylineaGT", pip=T)
+  print(reticulate::py_available())
 }
