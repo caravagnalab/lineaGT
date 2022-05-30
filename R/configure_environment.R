1d6aad79065b505d57e0d0e582e62b86ade38e4d
elena-buscaroli <ele.buscaroli@gmail.com>
2022-05-30 19:28:38 +0200

    reticulate configuration

--- R/configure_environment.R
+++ R/configure_environment.R
@@ -11,25 +11,26 @@
 #' @export configure_environment
 
 
-configure_environment = function(envname="lineagt-env") {
-  if (interactive()) {
-    # install miniconda if needed
-    check_conda()
+configure_environment = function(envname="lineagt-env", use_default=F) {
+  # install miniconda if needed
+  check_conda(use_default=use_default)
 
-    envname = check_conda_env(envname=envname)
+  envname = check_conda_env(envname=envname, use_default=use_default)
 
-    check_python_deps(envname=envname)
-  }
-
+  check_python_deps(envname=envname, use_default=use_default)
 }
 
 
-check_conda = function() {
+check_conda = function(use_default=F) {
   if (!have_conda()) {
     cat("It is not prossibe to find a Anaconda or Miniconda version installed.\n")
-    cat("The Miniconda installer will be downloaded and used to install Miniconda. Proceed?\n")
-    cat("(yes/no)\n")
-    answ = readline()
+
+    if (use_default) answ = "yes"
+    else {
+      cat("The Miniconda installer will be downloaded and used to install Miniconda. Proceed?\n")
+      cat("(yes/no)\n")
+      answ = readline()
+    }
 
     if (answ == "yes") install_miniconda_lineagt()
     else { cat("Miniconda will not be installed."); return() }
@@ -37,12 +38,22 @@ check_conda = function() {
 }
 
 
-check_conda_env = function(envname="lineagt-env") {
+check_conda_env = function(envname="lineagt-env", use_default=F) {
+  if (have_loaded_env()) {
+    envname = sapply(reticulate::conda_list()$name, grepl, reticulate::py_discover_config()$python) %>%
+      which() %>% names()
+    return(envname)
+  }
+
   if (!have_conda_env("lineagt-env")) {
     cat("The environment 'lineagt-env' is not present.\n")
-    cat("Do you want to load an existing environment, to create a new one named 'lineagt-env' or to cancel?\n")
+
+    if (use_default) answ = "create"
+    else {
+      cat("Do you want to load an existing environment, to create a new one named 'lineagt-env' or to cancel?\n")
     cat("(load/create/cancel)\n")
     answ = readline()
+    }
 
     if (answ == "create") {
       envname = "lineagt-env"
@@ -55,11 +66,8 @@ check_conda_env = function(envname="lineagt-env") {
       return()
     }
   } else {
-    cat("The environment 'lineagt-env' is already present!\n")
-    cat("Do you want to load it or do you want to load another environment 'envname'?\n")
-    cat("(load/envname)\n")
-    answ = readline()
-    if (answ != "load") envname = readline()
+    # cat("The environment 'lineagt-env' is already present and will be loaded!\n")
+    envname = "lineagt-env"
   }
 
   use_conda_env(envname)
@@ -68,12 +76,21 @@ check_conda_env = function(envname="lineagt-env") {
 }
 
 
-check_python_deps = function(envname="lineagt-env") {
+have_loaded_env = function() {
+  return(grepl("envs", reticulate::py_discover_config()$python))
+}
+
+
+check_python_deps = function(envname="lineagt-env", use_default=F) {
   if (!have_python_deps(envname)) {
     cat(paste("The required Python packages are not installed in the '", envname, "' environment.\n", sep=""))
-    cat("Proceed with the installation?\n")
-    cat("(yes/no)\n")
-    answ = readline()
+
+    if (use_default) answ = "yes"
+    else {
+      cat("Proceed with the installation?\n")
+      cat("(yes/no)\n")
+      answ = readline()
+    }
 
     if (answ == "yes") install_python_deps(envname)
     else return()
--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   # unset reticulate python environment, for more details, see:
   # https://github.com/greta-dev/greta/issues/444
-  Sys.unsetenv("RETICULATE_PYTHON")
+  # Sys.unsetenv("RETICULATE_PYTHON")
 
-  # if (have_lineagt_conda_env()) {
-  #   cat("The conda environment 'lineagt_env' is present and will be loaded!")
-  #   use_conda_env()
-  # } else {
-  configure_environment()
-  # }
+  if (interactive()) configure_environment()
+  else configure_environment(use_default=T)
 
 }
