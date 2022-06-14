bd64dbaa97ea30a673b36d31d1fdb3d710262db4
elena-buscaroli <ele.buscaroli@gmail.com>
2022-06-14 15:19:49 +0200

    updates in tree visualization

--- R/plots_trees.R
+++ R/plots_trees.R
@@ -1,16 +1,18 @@
-plot_phylogeny = function(x, min_frac=0, highlight=c(), vaf.df=NULL) {
+plot_phylogeny = function(x, min_frac=0, highlight=c(), vaf.df=NULL, label="") {
 
   if (purrr::is_empty(highlight)) highlight = x %>% get_unique_labels()
   clusters_joined = intersect(select_relevant_clusters(x, min_frac), highlight)
 
+  if (label!="") nn = paste("trees", label, sep=".")
+
   if (!"trees" %in% names(x)) {
-    x = fit_phylogenies(x, vaf.df=vaf.df, min_frac=min_frac, highlight=highlight)
+    x = fit_phylogenies(x, vaf.df=vaf.df, min_frac=min_frac, highlight=highlight, label=label)
   }
 
   tree_plots = list()
   for (cluster in clusters_joined) {
     if (!purrr::is_empty(x$trees[[cluster]])) {
-      tree = x$trees[[cluster]][[1]]
+      tree = x[[nn]][[cluster]][[1]]
       tree_plots[[cluster]] = ctree:::plot.ctree(tree) + labs(title=cluster)
     }
   }
