88cfdcea66f243d65648f21fbbc6a803ff394f42
Giulio Caravagna <gcaravagn@gmail.com>
2022-06-22 08:06:22 +0200

    S3 methods (draft)

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
 .onLoad <- function(libname, pkgname) {
+
+  pk = 'lineaGT'
+  pk_l = 'Lineage inference from gene therapy'
+  www = "https://caravagnalab.github.io/lineaGT/"
+
+  cli::cli_alert_success(
+    'Loading {.field {pk}}, {.emph \'{pk_l}\'}. Support : {.url { www}}' )
+
   # unset reticulate python environment, for more details, see:
   # https://github.com/greta-dev/greta/issues/444
   # Sys.unsetenv("RETICULATE_PYTHON")
